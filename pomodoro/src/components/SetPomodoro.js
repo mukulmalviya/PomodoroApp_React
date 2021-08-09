46d1446a1020b26a42b8fb6104482c0d429121ac
Mukul Malviya <mukulmalviya55@gmail.com>
2021-08-09 16:18:19 +0530

    pomodoro complete

---
+++ pomodoro/src/components/SetPomodoro.js
@@ -0,0 +1,60 @@
+import React, { useContext, useState } from 'react'
+import { SettingsContext } from '../context/SettingsContext'
+
+const SetPomodoro = () => {
+
+    const [newTimer, setNewTimer] = useState({
+        work: 50,
+        short: 10,
+        long: 25,
+        active: 'work' 
+    })
+
+    const {updateExecute} = useContext(SettingsContext)
+
+    const handleChange = input => {
+        const {name, value} = input.target
+        switch (name) {
+            case 'work':
+                setNewTimer({
+                    ...newTimer,
+                    work: parseInt(value)
+                })
+                break;
+            case 'shortBreak':
+                setNewTimer({
+                    ...newTimer,
+                    short: parseInt(value)
+                })
+                break;
+            case 'longBreak':
+                setNewTimer({
+                    ...newTimer,
+                    long: parseInt(value)
+                })
+                break;
+        }
+    }
+    const handleSubmit = e => {
+        e.preventDefault()
+        updateExecute(newTimer)
+    }
+    return (
+        <div className="form-container">
+            <form noValidate onSubmit={handleSubmit}>
+                <div className="input-wrapper">
+                    <input className="input" type="number" name="work" onChange={handleChange} value={newTimer.work} />
+                    <input className="input" type="number" name="shortBreak" onChange={handleChange} value={newTimer.short} />
+                    <input className="input" type="number" name="longBreak" onChange={handleChange} value={newTimer.long} />
+                </div>
+                <button type='submit'>Set Timer</button>
+            </form>
+            <footer>
+  <p>Made with 💚 by Mukul Malviya</p>
+  <p><a href="https://github.com/mukulmalviya/PomodoroApp_React">Github link</a></p>
+</footer> 
+        </div>
+    )
+}
+
+export default SetPomodoro
